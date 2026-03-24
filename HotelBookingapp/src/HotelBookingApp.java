ba9e5a64e878c6bfb08d5d925fe654a6d1ffe471
nandaa777 <kishorereddyty@gmail.com>
2026-03-24 14:49:25 +0530

    uc1

---
+++ HotelBookingapp/src/HotelBookingApp.java
@@ -0,0 +1,13 @@
+public class HotelBookingApp {
+    public static void main(String[] args) {
+
+        // Display welcome message
+        System.out.println("====================================");
+        System.out.println(" Welcome to Book My Stay Application ");
+        System.out.println(" Hotel Booking Management System ");
+        System.out.println(" Version: 1.0 ");
+        System.out.println("====================================");
+
+        // Application execution ends
+    }
+}
