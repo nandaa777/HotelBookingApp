d74c0002fc499347655b36c9d2ba8ac240e8f355
nandaa777 <kishorereddyty@gmail.com>
2026-03-24 15:04:14 +0530

    uc2

--- HotelBookingapp/src/HotelBookingApp.java
+++ HotelBookingapp/src/HotelBookingApp.java
@@ -1,13 +1,95 @@
-public class HotelBookingApp {
+
+abstract class Room {
+
+    private String roomType;
+    private int beds;
+    private double size;
+    private double price;
+
+    public Room(String roomType, int beds, double size, double price) {
+        this.roomType = roomType;
+        this.beds = beds;
+        this.size = size;
+        this.price = price;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public int getBeds() {
+        return beds;
+    }
+
+    public double getSize() {
+        return size;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void displayRoomDetails() {
+        System.out.println("Room Type : " + roomType);
+        System.out.println("Beds      : " + beds);
+        System.out.println("Size      : " + size + " sq.ft");
+        System.out.println("Price     : $" + price);
+    }
+}
+
+class SingleRoom extends Room {
+
+    public SingleRoom() {
+        super("Single Room", 1, 200, 100);
+    }
+}
+
+class DoubleRoom extends Room {
+
+    public DoubleRoom() {
+        super("Double Room", 2, 350, 180);
+    }
+}
+
+class SuiteRoom extends Room {
+
+    public SuiteRoom() {
+        super("Suite Room", 3, 500, 300);
+    }
+}
+
+
+public class HotelBookingApp{
+
     public static void main(String[] args) {
 
-        // Display welcome message
-        System.out.println("====================================");
-        System.out.println(" Welcome to Book My Stay Application ");
-        System.out.println(" Hotel Booking Management System ");
-        System.out.println(" Version: 1.0 ");
-        System.out.println("====================================");
+        System.out.println("===== Welcome to Book My Stay =====");
+        System.out.println("Room Availability\n");
 
-        // Application execution ends
+        // Polymorphic references
+        Room singleRoom = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suiteRoom = new SuiteRoom();
+
+        int singleRoomAvailability = 5;
+        int doubleRoomAvailability = 3;
+        int suiteRoomAvailability = 2;
+
+        System.out.println("---- Single Room Details ----");
+        singleRoom.displayRoomDetails();
+        System.out.println("Available Rooms: " + singleRoomAvailability);
+        System.out.println();
+
+        System.out.println("---- Double Room Details ----");
+        doubleRoom.displayRoomDetails();
+        System.out.println("Available Rooms: " + doubleRoomAvailability);
+        System.out.println();
+
+        System.out.println("---- Suite Room Details ----");
+        suiteRoom.displayRoomDetails();
+        System.out.println("Available Rooms: " + suiteRoomAvailability);
+        System.out.println();
+
+        System.out.println("Thank you for using Book My Stay!");
     }
 }
